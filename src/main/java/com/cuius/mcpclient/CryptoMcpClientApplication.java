5aaa158343d4a587843848c5ca5805d97cd573ff
Terrence T Munyunguma <terrence.munyunguma@ibm.com>
2025-11-14 01:15:19 +0000

    init

---
+++ src/main/java/com/cuius/mcpclient/CryptoMcpClientApplication.java
@@ -0,0 +1,13 @@
+package com.cuius.mcpclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoMcpClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoMcpClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cuius/mcpclient/CryptoMcpClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cuius.mcpclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoMcpClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
